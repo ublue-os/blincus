0a9a722067e76e07bc9a703e25c552ea7d98029a
Brian Ketelsen <bketelsen@gmail.com>
2023-12-14 13:51:47 -0500

    fix: better error message on sanity check

--- src/lib/sanity.sh
+++ src/lib/sanity.sh
@@ -4,16 +4,22 @@ sanity() {
 if ! grep -q 'root:1000:1' /etc/subgid; then 
  errors=1;
  echo "Error: 'root:1000:1' missing from /etc/subgid";
+ echo "To resolve run:"
+ echo 'echo "root:1000:1" | sudo tee -a /etc/subuid /etc/subgid'
 fi
 
 if ! grep -q 'root:1000:1' /etc/subuid; then 
     errors=1;
     echo "Error: 'root:1000:1' missing from /etc/subuid";
+    echo "To resolve run:"
+    echo 'echo "root:1000:1" | sudo tee -a /etc/subuid /etc/subgid'
 fi
 
 if ! groups $USER | grep -qw 'incus-admin' ; then
     errors=1;
     echo "Error: User does not belong to 'incus-admin' group.";
+    echo "To resolve run:"
+    echo 'sudo usermod -aG $USER'
 fi
 if (( errors > 0 )); then
    echo "$(red Sanity check failed.)"
