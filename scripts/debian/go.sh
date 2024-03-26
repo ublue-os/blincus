be5380853ccfd68aa53d411142050cb98183debf
Brian Ketelsen <bketelsen@gmail.com>
2024-03-25 20:19:20 -0400

    chore: update go

--- scripts/debian/go.sh
+++ scripts/debian/go.sh
@@ -1,9 +1,9 @@
 #!/bin/env bash
 #
 sudo apt-get install wget
-wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
 
 sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
 
-echo export PATH=$PATH:/usr/local/go/bin:"$HOME"/go/bin >>~/.profile
+echo export PATH=$PATH:/usr/local/go/bin >>~/.profile
